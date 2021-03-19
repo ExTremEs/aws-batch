d9ea311383455f89d78f4775fb02e1856321f2f3
Thapabud Budnean <thapabud.budnean@scb.co.th>
2021-03-19 14:48:50 +0700

    Initial project.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        out.println("Hello from main method, Current time's " + new Date());
+    }
+}
